36eb10d038328befca97ee5ae0d6b96e2bc01a5f
Josh Broomberg <joshbroomberg@gmail.com>
2019-08-29 21:36:45 +0100

    move constants to file and add state management tooling - closes #10

---
+++ vars/scripts/constants.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+REMOTE_DATA_PATH="/home/ubuntu/work"
+CONTAINER_DATA_PATH="/home/jovyan/work"
+JUPYTER_CONTAINER_NAME="jupyter_server"
---
+++ vars/variables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source $EASY_JUPYTER_PATH/vars/scripts/constants.sh
+source $EASY_JUPYTER_PATH/vars/scripts/state_management.sh
