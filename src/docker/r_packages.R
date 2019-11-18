2cc4b43d4c238417fee56e7843068f5c2ebcab5b
Josh Broomberg <joshbroomberg@gmail.com>
2019-11-18 12:02:40 +0000

    upgraded R version and created R install mechanisms

---
+++ src/docker/r_packages.R
@@ -0,0 +1,25 @@
+options(repos=structure(c(CRAN="https://cloud.r-project.org/")))
+
+pkgLoad <- function(packages) {
+    packagecheck <- match( packages, utils::installed.packages()[,1] )
+
+    packagestoinstall <- packages[ is.na( packagecheck ) ]
+
+    if( length( packagestoinstall ) > 0L ) {
+        utils::install.packages(packagestoinstall)
+    } else {
+        print( "All requested packages already installed" )
+    }
+
+    for( package in packages ) {
+        suppressPackageStartupMessages(
+            library( package, character.only = TRUE, quietly = TRUE )
+        )
+    }
+
+}
+
+pkgLoad(c())
+
+library(devtools)
+devtools::install_github("soerenkuenzel/causalToolbox")
